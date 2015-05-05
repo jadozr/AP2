eaee8f99a56122f74dad67b6a83d66b3aaf8227d
James <jadozr@gmail.com>
2015-05-05 15:37:28 +0200

    Mise en place du tableau de voitures sans CSS

--- client/app/vehicules/vehicules.controller.js
+++ client/app/vehicules/vehicules.controller.js
@@ -1,15 +1,80 @@
 'use strict';
 
 angular.module('autoPrivilegeApp')
-  .controller('VehiculesCtrl', function ($scope,$http, $state) {
+  .controller('VehiculesCtrl', function ($scope, $q, $http, $state, $filter, ngTableParams) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/cars').success(function (awesomeThings) {
+    var qDocs = $q.defer();
+    qDocs.resolve($http.get('/api/cars').success(function (awesomeThings) {
       $scope.message = awesomeThings;
-    });
+      $scope.tableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 12           // count per page
+      }, {
+        total: qDocs.length, // length of data
+        getData: function ($defer, params) {
+          qDocs.promise.then(function (result) {
+            // use build-in angular filter
+            var orderedData = params.sorting ?
+              $filter('orderBy')(result.data, params.orderBy()) :
+              result.data;
+            orderedData = params.filter ?
+              $filter('filter')(orderedData, params.filter()) :
+              orderedData;
+            $scope.users = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+            $scope.totalsCars = orderedData.length;
+            if (orderedData.slice(0, 3).length === 1) {
+              $scope.colonne1 = true;
+            } else {
+              $scope.colonne1 = false;
+            }
+            if (orderedData.slice(0, 3).length === 2) {
+              $scope.colonne2 = true;
+            } else {
+              $scope.colonn2 = false;
+            }
+            $defer.resolve($scope.users);
+          });
+        }
+      });
+    }));
+
+    var inArray = Array.prototype.indexOf ?
+      function (val, arr) {
+        return arr.indexOf(val);
+      } :
+      function (val, arr) {
+        var i = arr.length;
+        while (i--) {
+          if (arr[i] === val) {
+            return i;
+          }
+        }
+        return -1;
+      };
+
+    $scope.names = function (column) {
+      var def = $q.defer(),
+        arr = [],
+        names = [];
+      qDocs.promise.then(function (result) {
+        angular.forEach(result.data, function (item) {
+          if (inArray(item.Marque, arr) === -1) {
+            arr.push(item.Marque);
+            names.push({
+              'id': item.Marque,
+              'title': item.Marque
+            });
+          }
+        });
+      });
+      def.resolve(names);
+      return def;
+    };
 
     // Show Car detail
     $scope.showCarDetail = function (_id) {
       $state.go('vehiculeDetail', {id: _id});
     };
+
   });
