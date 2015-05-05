01cb35d321745c5c00d6e8256fbca516d826d833
James <jadozr@gmail.com>
2015-05-05 15:47:34 +0200

    modification cars.spec pour les tests

--- server/api/cars/cars.spec.js
+++ server/api/cars/cars.spec.js
@@ -4,11 +4,11 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('GET /api/carss', function() {
+describe('GET /api/cars', function() {
 
   it('should respond with JSON array', function(done) {
     request(app)
-      .get('/api/carss')
+      .get('/api/cars')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
